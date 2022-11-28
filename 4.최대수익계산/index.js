fdbe1bbd3253c5d7979975278ec24757c3a4e1ad
baedollee <baesee0401@gmail.com>
2022-11-28 16:17:38 +0900

    11/28

--- 4.최대수익계산/index.js
+++ 4.최대수익계산/index.js
@@ -6,6 +6,20 @@
 // - 예) 기간중 가장 쌀 때 주식을 구입, 가장 비쌀 때 주식을 판매
 // - 예) 3일 동안의 주식가격이 [100. 50. 250] 이라면, 최대이익은 200
 
-function(){
-    
+function maxProfit(prices) {
+  let maxProfit = 0;
+  let minBuyPrice = prices[0];
+
+  for (let price of prices) {
+    if (minBuyPrice < price) {
+      maxProfit = Math.max(maxProfit, price - minBuyPrice);
+    } else {
+      minBuyPrice = price;
+    }
+  }
+  return maxProfit;
 }
+
+const test1 = [10, 20, 50, 100, 80, 250]; // 240
+
+console.log(maxProfit(test1));
